94cb54dd3d1549dcb92467631ceac5d4e434c7ee
Forrest Fleming <ffleming@gmail.com>
2016-05-20 19:50:36 -0700

    Move root to its own file

--- lib/transform_tree.rb
+++ lib/transform_tree.rb
@@ -1,31 +1,7 @@
 require "transform_tree/version"
 require "transform_tree/transform_node"
+require "transform_tree/transform_root"
 
 module TransformTree
   NULL = ->(o){o}
-  class TransformRoot < TransformNode
-    def initialize
-      super(TransformTree::NULL, 0)
-    end
-
-    def add_transform(*closures)
-      super(*closures)
-    end
-
-    def report
-      super
-    end
-
-    def height
-      leaves.first.level + 1
-    end
-
-    def execute(*args)
-      super(*args)
-    end
-
-    alias_method :add_transforms, :add_transform
-
-  end
-
 end
---
+++ lib/transform_tree/transform_root.rb
@@ -0,0 +1,25 @@
+module TransformTree
+  class TransformRoot < TransformNode
+    def initialize
+      super(TransformTree::NULL, 0)
+    end
+
+    def add_transform(*closures)
+      super(*closures)
+    end
+
+    def report
+      super
+    end
+
+    def height
+      leaves.first.level + 1
+    end
+
+    def execute(*args)
+      super(*args)
+    end
+
+    alias_method :add_transforms, :add_transform
+  end
+end
