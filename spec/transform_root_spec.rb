42d5222520af359900748ea5e3a8d8159f13caad
Forrest Fleming <ffleming@gmail.com>
2016-05-20 20:10:17 -0700

    format long string

--- spec/transform_root_spec.rb
+++ spec/transform_root_spec.rb
@@ -33,7 +33,12 @@ RSpec.describe TransformTree::TransformRoot do
 
   describe '#report' do
     it 'should ouput correctly' do
-      huge_expected = "0\n 1\n  2\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n  2\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n"
+      huge_expected = "0\n 1\n  2\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n" <<
+        "    4\n     5\n      6\n      6\n     5\n      6\n      6\n   3\n    4\n     5\n      6\n" <<
+        "      6\n     5\n      6\n      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n" <<
+        "  2\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n    4\n     5\n      6\n" <<
+        "      6\n     5\n      6\n      6\n   3\n    4\n     5\n      6\n      6\n     5\n      6\n" <<
+        "      6\n    4\n     5\n      6\n      6\n     5\n      6\n      6\n"
       aggregate_failures do
         expect(single.report).to eq "0\n"
         expect(two.report).to eq "0\n 1\n"
