6feebce7a5264743b831c9ceaa653a20f280717a
Forrest Fleming <ffleming@gmail.com>
2016-05-22 09:37:00 -0700

    Use Code Climate's test reporter to generate HTML
    No use in doubling up on simplecov's code (since CC uses it anyway)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
-require "simplecov"
 require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-SimpleCov.start
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ])
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'transform_tree'
